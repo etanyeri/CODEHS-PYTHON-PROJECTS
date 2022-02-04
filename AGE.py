57b8147ceb6ac5cdcfc7ec0bd9f6a7b0df48f4ef
etanyeri <94400650+etanyeri@users.noreply.github.com>
2022-02-03 23:40:56 -0800

    AGE

---
+++ AGE.py
@@ -0,0 +1,6 @@
+"""Write a program that asks them how old they are. 
+Once they enter their age, print how many candles they’ll need for their birthday cake.
+(This should be one more than their current age.)"""
+
+age=int(input('How old are you?:'))
+print('You will need this many candles for your birthday cake:',age+1)
