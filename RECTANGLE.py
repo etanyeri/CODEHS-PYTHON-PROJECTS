ef08ed7979698a81c3272f0ac239214794e818cb
etanyeri <94400650+etanyeri@users.noreply.github.com>
2022-02-09 20:52:31 -0800

    rectangle area

---
+++ RECTANGLE.py
@@ -0,0 +1,15 @@
+
+
+""" write a program to compute 
+the area and perimeter of the rectangle and save these values
+inside variables named area and perimeter."""
+
+
+#  set length to 10 and width to 5.
+length= 10
+width= 5
+
+area= length*width
+perimeter= 2*(length+width)
+print(area)
+print(perimeter)
