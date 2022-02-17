62f54b391e5916040b900fc0089405508058c465
etanyeri <94400650+etanyeri@users.noreply.github.com>
2022-02-16 21:17:20 -0800

    rectangle part-2

---
+++ RECTANGLE.PART2.py
@@ -0,0 +1,13 @@
+
+
+"""This program  prints out the names of each number being printed.""" 
+
+#ength is set to 10 and width is set to 5.
+length= 10
+width= 5
+
+area= length*width
+perimeter= 2*(length+width)
+
+print('Area:'+ str(area))
+print('Perimeter:' +str(perimeter))
