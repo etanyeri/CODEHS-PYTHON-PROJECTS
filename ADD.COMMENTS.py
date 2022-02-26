c8fccfcd0bf45a62122cb10979bbcc63e33f6dbe
etanyeri <94400650+etanyeri@users.noreply.github.com>
2022-02-25 22:49:36 -0800

    ADD COMMENT

---
+++ ADD.COMMENTS.py
@@ -0,0 +1,16 @@
+
+"""this program's purpose is to get a full and last name by 
+asking a user's input"""
+
+#First ask the user their first name
+first_name = input("Enter your first name: ")
+
+#And get an input of the middle name
+middle_name = input("Enter your middle name: ")
+
+#and ask user's last name.
+last_name = input("Enter your last name: ")
+
+
+full_name = first_name + " " + middle_name + " " + last_name
+print(full_name)
