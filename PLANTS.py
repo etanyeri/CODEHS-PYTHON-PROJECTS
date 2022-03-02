04bcf12236207c73601e094008158a209a79f71b
etanyeri <94400650+etanyeri@users.noreply.github.com>
2022-03-01 22:35:03 -0800

    plants

---
+++ PLANTS.py
@@ -0,0 +1,14 @@
+
+
+
+"""A boolean variable called needs_water exists that represents 
+whether or not one of your plants needs water. 
+Make another boolean variable called needs_to_be_repotted 
+ whether or not you need to repot one of your plants."""
+ 
+ 
+needs_water = True
+needs_to_be_repotted = False
+
+print("Needs water: " + str(needs_water))
+print('Needs to be repotted: ' +  str(needs_to_be_repotted))
