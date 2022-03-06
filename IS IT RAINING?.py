88b0e6b7c521f73807df5376633e95728e5b2bde
etanyeri <94400650+etanyeri@users.noreply.github.com>
2022-03-05 20:56:57 -0800

    rains outside

---
+++ IS IT RAINING?.py
@@ -0,0 +1,9 @@
+"""This  program that has a Boolean variable that represents 
+whether or not it is raining outside."""
+
+rains_outside= False
+
+if rains_outside:
+    print("I'm going to dance in the rain!")
+else:
+    print("I'm going to dance in the sun!")
